9d563556c4b1d4dac6df37accf738021a5940b16
joehuang <xjoe831028@gmail.com>
2021-12-22 10:43:41 +0800

    新增 jpa  api

--- src/main/java/com/example/demo/entity/Time.java
+++ src/main/java/com/example/demo/entity/Time.java
@@ -22,22 +22,16 @@ public class Time implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
-//	private Long timeId;
-	
 	/**
 	 * ex: Oct 6, 2021 10:58:00 UTC
 	 */
 	@ApiModelProperty("updated 修改日期")
-//	@Column
 	private String updated;
 	
 	/**
 	 * ex: 2021-10-06T10:58:00+00:00
 	 */
 	@ApiModelProperty("updatedISO 修改日期ISO")
-//	@Column
 	private String updatedISO;
 	
 	/**
@@ -45,7 +39,6 @@ public class Time implements Serializable {
 	 * ex: Oct 6, 2021 at 11:58 BST
 	 */
 	@ApiModelProperty("updateduk 修改日期")
-//	@Column
 	private String updateduk;
 
 }
