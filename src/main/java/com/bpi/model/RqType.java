fd365bf309e6f93ef2ed7d8ac6ac20a6980346da
joehuang <xjoe831028@gmail.com>
2023-03-31 16:55:14 +0800

    add rqType description

--- src/main/java/com/bpi/model/RqType.java
+++ src/main/java/com/bpi/model/RqType.java
@@ -12,6 +12,16 @@ import java.lang.annotation.Target;
  * 
  * @Date 2022/02/14
  *
+ * @Retention
+ * 	SOURCE代表的是這個Annotation類型的信息只會保留在程序源碼里，源碼如果經過了編譯之后，Annotation的數據就會消失,并不會保留在編譯好的.class文件里面。
+ * 	ClASS的意思是這個Annotation類型的信息保留在程序源碼里，同時也會保留在編譯好的.class文件里面，在執行的時候，并不會把這一些信息加載到虛擬機(JVM)中去.注意一下，當你沒有設定一個Annotation類型的Retention值時，系統默認值是CLASS.
+ * 	RUNTIME表示在源碼、編譯好的.class文件中保留信息，在執行的時候會把這一些信息加載到JVM中去的．
+ * 
+ * @Target : 限定註解的使用位置
+ * 	ANNOTATION_TYPE: 註釋型態
+ * 	FIELD: 用於資料成員
+ *  METHOD: 方法
+ *  PARAMETER: 方法參數
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.ANNOTATION_TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER })
